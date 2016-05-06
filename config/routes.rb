9e2b8ab8d1a81d5013fe7a6d1e7f356f932dd7e9
glory <glory.angelova@gmail.com>
2016-05-06 14:20:27 +0200

    user workign

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   devise_for :users
   resources :words
   resources :articles
-  root 'layout#welcome'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
