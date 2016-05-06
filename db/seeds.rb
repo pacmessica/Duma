0913ef375a4d4831675846d13244701355e1c67f
pacmessica <jz.paczuski@gmail.com>
2016-05-06 16:45:23 +0200

    Articleshowpage (#6)
    * add article show page
    
    * improve seeds
    
    * show article content

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Word.destroy_all
+Article.destroy_all
+User.destroy_all
+
 users = [
   User.create( email: "iulia@ex.com", password: "abcd1234" ),
   User.create( email: "glory@ex.com", password: "abcd1234" ),
@@ -8,11 +12,19 @@ users.each do |user|
   5.times do
     article = Article.create(content:Faker::Lorem.paragraph, title:Faker::Lorem.sentence, image:Faker::Avatar.image ,user: user)
     article.content.split(" ").each do |word|
-      Word.create(name:word, known:true, user:user)
+      word.downcase!
+      if Word.where( name:word).where(user:user).exists?
+      else
+        Word.create(name:word, known:true, user:user)
+      end
     end
-  end
-  article2 = Article.create(content:Faker::Lorem.paragraph, title:Faker::Lorem.sentence, image:Faker::Avatar.image ,user: user)
-  article2.content.split(" ").each do |word|
-    Word.create(name:word, known:false, translation:Faker::Hacker.noun, user:user)
+    article2 = Article.create(content:Faker::Lorem.paragraph, title:Faker::Lorem.sentence, image:Faker::Avatar.image ,user: User.first)
+    article2.content.split(" ").each do |word|
+      word.downcase!
+      if Word.where( name:word).where(user:user).exists?
+      else
+        Word.create(name:word, known:false, translation:Faker::Hacker.noun, user:user)
+      end
+    end
   end
 end
