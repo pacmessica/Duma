605a3023865d32aae7cfeae56f9d6b7c3cceb084
iulia0103 <iulia.leancu@gmail.com>
2016-05-06 19:01:09 +0200

    change known attribute to true for study words, by pressimg the button

--- app/assets/javascripts/words.js
+++ app/assets/javascripts/words.js
@@ -3,4 +3,22 @@ $(document).ready(function(){
    $('h3').on('click', function(){
 	  $(this).next('p').toggle();
    });
+
+   $('button').on('click', function(){
+     var wordId = this.dataset.wordid;
+     var button = this;
+     $.ajax({
+      type: "PATCH",
+      async: true,
+      complete: function(result) {
+        console.log(result)
+        if ( result.responseText === 'success' ) {
+          console.log($(button).parent().parent().remove());
+        }
+      },
+      url: "/words/"+ wordId,
+      data: JSON.stringify({ known: true }),
+      contentType: "application/json",
+      dataType: "json"});
+   });
 });
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,12 +1,10 @@
 class WordsController < ApplicationController
   def index
-    @study_words = Word.study_words.all
-    @known_words = Word.known_words.all
+    @words = Word.study_words.all
   end
 
   def show
-    @study_word = Word.find(params[:id])
-    @known_word = Word.find(params[:id])
+    @word = Word.find(params[:id])
   end
 
   def new
@@ -16,24 +14,13 @@ class WordsController < ApplicationController
   end
 
   def edit
-    @study_word = Word.find(params[:id])
-    @known_word = Word.find(params[:id])
+    @word = Word.find(params[:id])
   end
 
   def update
-    @study_word = Word.find(params[:id])
-    @known_word = Word.find(params[:id])
+    @word = Word.find(params[:id])
 
-    if @study_word.update_attributes( study_word_params )
-     redirect_to @study_words
-    else
-       render 'edit'
-    end
-
-    if @known_word.update_attributes( known_word_params )
-     redirect_to @known_words
-    else
-       render 'edit'
-    end
+    @word.update_attributes( known: params[:known] )
+    render json: 'success'
   end
 end
