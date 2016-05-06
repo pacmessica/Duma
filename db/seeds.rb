b66ff6bd7a4193741b857d7e8c9a01454590ccb3
Jessica Paczuski <jz.paczuski@gmail.com>
2016-05-06 14:35:24 +0200

    add unknown word seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,8 @@ users.each do |user|
       Word.create(name:word, known:true, user:user)
     end
   end
+  article2 = Article.create(content:Faker::Lorem.paragraph, title:Faker::Lorem.sentence, image:Faker::Avatar.image ,user: user)
+  article2.content.split(" ").each do |word|
+    Word.create(name:word, known:false, translation:Faker::Hacker.noun, user:user)
+  end
 end
