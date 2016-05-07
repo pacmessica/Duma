e5b29f8d762323da10157ff70bf1af1bcea5ba3f
Jessica Paczuski <jz.paczuski@gmail.com>
2016-05-07 16:37:16 +0200

    add ability to add new words to db

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -4,58 +4,115 @@ function updateCounters() {
     // console.log(element.innerHTML)
     return element.innerHTML.toLowerCase();
   });
-  console.log(study_words)
+  // console.log(study_words)
   study_words = jQuery.unique( study_words );
-  console.log(study_words.length)
-  $("#study-words-count").html($(".study-word").length);
-  $("#known-words-count").html($(".known-word").length);
+
+
+  var known_words = $(".known-word").map(function(index, element){
+    return element.innerHTML.toLowerCase();
+  });
+  known_words = jQuery.unique( known_words );
+
+  $("#study-words-count").html(study_words.length);
+  $("#known-words-count").html(known_words.length);
   $("#new-words-count").html($(".word").length - $(".study-word").length - $(".known-word").length);
+}
+
+function createStudyWord(){
+  var element = this;
+  var wordTranslation = prompt("please enter translation for " + element.innerHTML);
+  console.log(wordTranslation);
+  var newStudyWord = { name: element.innerHTML.toLowerCase(), translation: wordTranslation, known: false }
+  console.log(newStudyWord);
+  $.ajax({
+    type: "POST",
+    complete: function(result) {
+      if ( result.statusText === 'OK' ) {
+        removeWordListeners()
+        console.log(result.responseJSON.word)
+        var wordId = result.responseJSON.word.id;
+        $(element)
+          .addClass("study-word")
+          .removeClass("new-word")
+          .attr("data-wordid", wordId)
+          .attr("data-translation", wordTranslation);
+        addWordListeners();
+        $(element).next('.translation').text(wordTranslation)
+      }
+      updateCounters();
+    },
+    url: "/words.json",
+    data: JSON.stringify({ word: newStudyWord }),
+    contentType: "application/json",
+    dataType: "json"
+  });
 }
 
 
 function toggleStudyKnownWord(){
+  $('.translation').hide();
   var wordId = this.dataset.wordid;
   var wordTranslation = this.dataset.translation;
   var element = this;
   var known = element.classList.contains("known-word");
+  console.log("toggled:", wordId)
 
   if (wordTranslation == "") {
-    prompt("please enter translation for " + element.innerHTML);
+    var wordTranslation = prompt("please enter translation for " + element.innerHTML);
   }
 
   $.ajax({
     type: "PATCH",
     complete: function(result) {
       if ( result.responseText === 'success' ) {
+        removeWordListeners()
         $('[data-wordid=' + wordId + ']')
           .toggleClass("study-word")
           .toggleClass("known-word");
+        addWordListeners();
       }
       updateCounters();
     },
     url: "/words/"+ wordId,
-    data: JSON.stringify({ known: !known }),
+    data: JSON.stringify({ known: !known, translation: wordTranslation }),
     contentType: "application/json",
     dataType: "json"
   });
 };
 
+function mouseEnterWord(){
+    $(this).next('.translation').show();
+}
+
+function mouseLeaveWord()  {
+  $(this).next('.translation').hide();
+}
+
+function addWordListeners(){
+  $('.study-word').on({
+    mouseenter: mouseEnterWord,
+    mouseleave: mouseLeaveWord,
+    click: toggleStudyKnownWord
+  });
+
+  $(".known-word").on("click", toggleStudyKnownWord);
+  $(".new-word").on("click", createStudyWord);
+}
+
+
+function removeWordListeners(){
+  $('.study-word').off({
+    mouseenter: mouseEnterWord,
+    mouseleave: mouseLeaveWord,
+    click: toggleStudyKnownWord
+  });
+
+  $(".known-word").off("click", toggleStudyKnownWord);
+  $(".new-word").off("click", createStudyWord);
+}
 
 $(document).ready(function() {
   $('.translation').hide();
-  $('.study-word').on({
-    mouseenter: function() {
-      $(this).next('.translation').show();
-    },
-    mouseleave: function() {
-      $(this).next('.translation').hide();
-    }
-  });
-
-
-
-
-  $(".known-word").bind("click", toggleStudyKnownWord);
-  $(".study-word").bind('click', toggleStudyKnownWord);
+  addWordListeners();
   updateCounters();
 });
