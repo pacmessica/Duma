5e6c7cec62a0a256a68b29ac12bcc311f89ee69f
Jessica Paczuski <jz.paczuski@gmail.com>
2016-05-09 15:35:32 +0200

    add a running count of known words to header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-
+  helper_method :current_user_known_words_count
+   def current_user_known_words_count
+     @current_user_known_words_count ||= current_user.words.known_words.count
+   end
 end
